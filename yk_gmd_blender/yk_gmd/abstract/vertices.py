e2c7cab7bea6d53dd0af32159eca19281f31a91f
SutandoTsukai181 <52977072+SutandoTsukai181@users.noreply.github.com>
2020-07-24 00:56:26 +0300

    Vertex Layout rewrite [Part 1 of 2]

--- yk_gmd_blender/yk_gmd/abstract/vertices.py
+++ yk_gmd_blender/yk_gmd/abstract/vertices.py
@@ -17,54 +17,118 @@ class GMDVertex:
     weights: Tuple[BoneWeight, BoneWeight, BoneWeight, BoneWeight]
     normal: Optional[Vec4]
     tangent: Optional[Vec4]
-    col0: Optional[Vec4]
-    col1: Optional[Vec4]
-    uv: Tuple[float, float]
+    diffuse: Optional[Vec4]
+    opacity: Optional[float]
+    specular: Optional[Vec4]
+    uv0: Tuple[float, float]
+    uv1: Tuple[float, float]
 
     def __init__(self):
         self.pos = None
         self.weights = None
         self.normal = None
         self.tangent = None
-        self.col0 = None
-        self.col1 = None
-        self.uv = None
+        self.diffuse = None
+        self.specular = None
+        self.uv0 = None
+        self.uv1 = None
 
 
 @dataclass(frozen=True)
 class GMDVertexBufferLayout:
-    pos_en: bool
-    weights_en: bool
-    normal_en: bool
-    tangent_en: bool
-    col0_en: bool
-    col1_en: bool
-    uv_en: bool
-    uv_elem_bytes: int  # uv components are 16 or 32 bit floating point
+    pos_type: int
+    weights_type: int
+    bones_en: int
+    normal_type: int
+    tangent_type: int
+    unk_type: int
+    diffuse_type: int
+    specular_type: int
+    uv_type: int
+
+
+    def get_vector_format_string(self, value):
+        format_string = ""
+        for i in range(value):
+            if value >= 3:
+                format_string += "f"
+            elif value == 2:
+                format_string += "ee"
+            else:
+                format_string += "BBBB"
+        return format_string
+
+    def get_type_format_string(self, name):
+        format_string = ">"
+        if name == "pos":
+            for i in range(self.pos_type):
+                format_string += "f" if self.pos_type - 2 > 0 else "e"
+        if name == "weights":
+            format_string += self.get_vector_format_string(self.weights_type)
+        if name == "bones":
+            format_string += "BBBB"
+        if name == "normal":
+            format_string += self.get_vector_format_string(self.normal_type)
+        if name == "tangent":
+            format_string += self.get_vector_format_string(self.tangent_type)
+        if name == "unk":
+            format_string += self.get_vector_format_string(self.unk_type)
+        if name == "diffuse":
+            format_string += self.get_vector_format_string(self.diffuse_type)
+        if name == "specular":
+            format_string += self.get_vector_format_string(self.specular_type)
+        if name == "uv":
+            index = [(self.uv_type >> 4, 0), (self.uv_type >> 12, 1), (self.uv_type >> 20, 2)]
+            index.sort(key=lambda tup: tup[0], reverse=True)
+            for i in range(len(index)):
+                if index[i][1] == 0:
+                    for t in range(self.uv_type & 0x0F):
+                        format_string += "BBxx"
+                    format_string += "/"
+                elif index[i][1] == 1:
+                    for t in range((self.uv_type >> 8) & 0x0F):
+                        format_string += "ee"
+                    format_string += "/"
+                else:
+                    for t in range((self.uv_type >> 16) & 0x0F):
+                        format_string += "ff"
+                    format_string += "/"
+        return format_string
 
     def calc_bytes_per_vertex(self):
         bpv = 0
-        if self.pos_en:
-            # pos is 32 bit floats
-            bpv += 4 * 3
-        if self.weights_en:
-            # bone id = 1 byte, weight = 1 byte fixed point
-            # 4 weights
-            bpv += 2 * 4
-        if self.normal_en:
-            # normal is 4 * 8bit fixed point
-            bpv += 4
-        if self.tangent_en:
-            # tangent is 4 * 8bit fixed point
-            bpv += 4
-        if self.col0_en:
-            # colors is 4 * 8bit fixed point
-            bpv += 4
-        if self.col1_en:
-            # colors is 4 * 8bit fixed point
-            bpv += 4
-        if self.uv_en:
-            bpv += 2 * self.uv_elem_bytes
+        if self.pos_type:
+            # pos can be 3 or 4 * 16bit or 32bit floats
+            bpv += 4 * self.pos_type if self.pos_type != 1 else 6
+        if self.weights_type:
+            # weights can be 4 * 16bit or 32bit floats, or 4 * 8bit fixed point
+            bpv += 4 * self.weights_type
+        if self.bones_en:
+            # bone ids are 4 * 1 byte
+            bpv += 4 * self.bones_en
+        if self.normal_type:
+            # normal can be 4 * 16bit or 3 * 32bit floats, or 4 * 8bit fixed point
+            bpv += 4 * self.normal_type
+        if self.tangent_type:
+            # tangent can be 4 * 16bit or 3 * 32bit floats, or 4 * 8bit fixed point
+            bpv += 4 * self.tangent_type
+        if self.unk_type:
+            # unk can be 4 * 16bit or 3 * 32bit floats, or 4 * 8bit fixed point
+            bpv += 4 * self.unk_type
+        if self.diffuse_type:
+            # diffuse can be 4 * 16bit or 32bit floats, or 4 * 8bit fixed point
+            # last float is opacity
+            bpv += 4 * self.diffuse_type
+        if self.specular_type:
+            # specular can be 4 * 16bit or 32bit floats, or 4 * 8bit fixed point
+            # last float is always null
+            bpv += 4 * self.specular_type
+        if self.uv_type:
+            # uv can be 2 * 16bit or 32bit floats, or 4 * 8bit fixed point
+            # there can be multiple uv for a single vertex
+            bpv += 4 * (self.uv_type & 0x0F)
+            bpv += 4 * ((self.uv_type >> 8) & 0x0F)
+            bpv += 8 * ((self.uv_type >> 16) & 0x0F)
         return bpv
 
     def unpack_vertices(self, vertex_count: int, data: bytes, start_offset=0) -> List[GMDVertex]:
@@ -76,37 +140,77 @@ class GMDVertexBufferLayout:
 
             vertex = GMDVertex()
             offset = 0
-            if self.pos_en:
-                vertex.pos = Vec3(*struct.unpack_from(">fff", vtx_data, offset=offset))
-                offset += struct.calcsize(">fff")
-            if self.weights_en:
-                weight_bytes = struct.unpack_from(">BBBBBBBB", vtx_data, offset=offset)
+            if self.pos_type:
+                format_string = self.get_type_format_string("pos")
+                vertex.pos = Vec3(*struct.unpack_from(format_string, vtx_data, offset=offset))
+                offset += struct.calcsize(format_string)
+            if self.weights_type:
+                format_string = self.get_type_format_string("weights")
+                if "B" in format_string:
+                    weight_bytes = Vec4(
+                        *[x / 255.0 for x in struct.unpack_from(format_string, vtx_data, offset=offset)])
+                else:
+                    weight_bytes = Vec4(*[x for x in struct.unpack_from(format_string, vtx_data, offset=offset)])
+                offset += struct.calcsize(format_string)
+                bone_bytes = struct.unpack_from(">BBBB", vtx_data, offset=offset)
                 vertex.weights = (
-                    BoneWeight(bone=weight_bytes[4 + 0], weight=weight_bytes[0] / 255.0),
-                    BoneWeight(bone=weight_bytes[4 + 1], weight=weight_bytes[1] / 255.0),
-                    BoneWeight(bone=weight_bytes[4 + 2], weight=weight_bytes[2] / 255.0),
-                    BoneWeight(bone=weight_bytes[4 + 3], weight=weight_bytes[3] / 255.0),
+                    BoneWeight(bone=bone_bytes[0], weight=weight_bytes[0]),
+                    BoneWeight(bone=bone_bytes[1], weight=weight_bytes[1]),
+                    BoneWeight(bone=bone_bytes[2], weight=weight_bytes[2]),
+                    BoneWeight(bone=bone_bytes[3], weight=weight_bytes[3]),
                 )
-                offset += struct.calcsize(">BBBBBBBB")
-            if self.normal_en:
-                vertex.normal = Vec4(*[((x / 255.0) * 2 - 1) for x in struct.unpack_from(">BBBB", vtx_data, offset=offset)])
                 offset += struct.calcsize(">BBBB")
-            if self.tangent_en:
-                vertex.tangent = Vec4(*[((x / 255.0) * 2 - 1) for x in struct.unpack_from(">BBBB", vtx_data, offset=offset)])
-                offset += struct.calcsize(">BBBB")
-            if self.col0_en:
-                vertex.col0 = Vec4(*[x / 255.0 for x in struct.unpack_from(">BBBB", vtx_data, offset=offset)])
-                offset += struct.calcsize(">BBBB")
-            if self.col1_en:
-                vertex.col1 = Vec4(*[x / 255.0 for x in struct.unpack_from(">BBBB", vtx_data, offset=offset)])
-                offset += struct.calcsize(">BBBB")
-            if self.uv_en:
-                if self.uv_elem_bytes == 2:
-                    fmt = ">ee"
-                elif self.uv_elem_bytes == 4:
-                    fmt = ">ff"
-                vertex.uv = struct.unpack_from(fmt, vtx_data, offset=offset)
-                offset += struct.calcsize(fmt)
+            if self.normal_type:
+                format_string = self.get_type_format_string("normal")
+                if "B" in format_string:
+                    vertex.normal = Vec4(
+                        *[((x / 255.0) * 2 - 1) for x in struct.unpack_from(format_string, vtx_data, offset=offset)])
+                else:
+                    vertex.normal = Vec4(
+                        *[x for x in struct.unpack_from(format_string, vtx_data, offset=offset)])
+                offset += struct.calcsize(format_string)
+            if self.tangent_type:
+                format_string = self.get_type_format_string("tangent")
+                if "B" in format_string:
+                    vertex.tangent = Vec4(
+                        *[((x / 255.0) * 2 - 1) for x in struct.unpack_from(format_string, vtx_data, offset=offset)])
+                else:
+                    vertex.tangent = Vec4(
+                        *[x for x in struct.unpack_from(format_string, vtx_data, offset=offset)])
+                offset += struct.calcsize(format_string)
+            if self.diffuse_type:
+                format_string = self.get_type_format_string("diffuse")
+                if "B" in format_string:
+                    vertex.diffuse = Vec4(
+                        *[x / 255.0 for x in struct.unpack_from(format_string, vtx_data, offset=offset)])
+                else:
+                    vertex.diffuse = Vec4(*[x for x in struct.unpack_from(format_string, vtx_data, offset=offset)])
+                offset += struct.calcsize(format_string)
+            if self.specular_type:
+                format_string = self.get_type_format_string("specular")
+                if "B" in format_string:
+                    vertex.specular = Vec4(
+                        *[x / 255.0 for x in struct.unpack_from(format_string, vtx_data, offset=offset)])
+                else:
+                    vertex.specular = Vec4(*[x for x in struct.unpack_from(format_string, vtx_data, offset=offset)])
+                offset += struct.calcsize(format_string)
+            if self.uv_type:
+                format_string = self.get_type_format_string("uv")
+                format_list = format_string.split("/")
+                if "B" in format_list[1]:
+                    vertex.uv0 = [x / 255.0 for x in struct.unpack_from(">" + format_list[1], vtx_data, offset=offset)]
+                else:
+                    vertex.uv0 = [x for x in struct.unpack_from(">" + format_list[1], vtx_data, offset=offset)]
+
+                offset += struct.calcsize(format_list[1])
+                vertex.uv1 = [0, 0]
+                if len(format_list) > 2:
+                    if "B" in format_list[2]:
+                        vertex.uv1 = [x / 255.0 for x in
+                                      struct.unpack_from(">" + format_list[2], vtx_data, offset=offset)]
+                    else:
+                        vertex.uv1 = [x for x in struct.unpack_from(">" + format_list[2], vtx_data, offset=offset)]
+                    offset += struct.calcsize(">" + format_list[2])
 
             vertices[i] = vertex
         return vertices
--- yk_gmd_blender/yk_gmd/structs/vertex_buffer_layout.py
+++ yk_gmd_blender/yk_gmd/structs/vertex_buffer_layout.py
@@ -9,11 +9,7 @@ class VertexBufferLayoutStruct(BaseBigEndianStructure):
     _fields_ = [
         ("id", c_uint32),
         ("vertex_count", c_uint32),
-
-        ("zero", c_ubyte * 3),
-        ("uv_flags", c_ubyte),
-        ("vertex_packing", c_uint32),
-
+        ("vertex_packing", c_uint64),
         ("vertex_data_start", c_uint32),
         ("vertex_data_length", c_uint32), # length in bytes
         ("bytes_per_vertex", c_uint32),
@@ -22,50 +18,120 @@ class VertexBufferLayoutStruct(BaseBigEndianStructure):
 
     @property
     def flags(self):
-        return [self.zero[0],self.zero[1],self.zero[3],self.uv_flags,
+        return [(int(self.vertex_packing)>>52)&0xFF,
+                (int(self.vertex_packing)>>48)&0xFF,
+                (int(self.vertex_packing)>>40)&0xFF,
+                (int(self.vertex_packing)>>32)&0xFF,
                 (int(self.vertex_packing)>>24)&0xFF,
                 (int(self.vertex_packing)>>16)&0xFF,
                 (int(self.vertex_packing)>>8)&0xFF,
-                (int(self.vertex_packing)>>0)&0xFF,
+                (int(self.vertex_packing)>>0)&0xFF
                 ]
 
-    def all_bits(self, data, bitmask):
-        return (data & bitmask) == bitmask
+    def get_vector_type(self, shift):
+        vector_type = (int(self.vertex_packing) >> shift) & 3
+        if vector_type == 0:
+            # 4 = Vector4
+            # 3 = Vector3
+            return 4
+        elif vector_type == 1:
+            # 2 = Vector4Half
+            return 2
+        else:
+            # 1 = 4 * 8bit fixed point
+            return 1
+
+    def get_property_type(self, name):
+        vertex_packing = int(self.vertex_packing)
+        if name == "pos":
+            pos_count = vertex_packing & 7
+            if (vertex_packing >> 3) & 1:
+                # 1 = Vector3Half (unlikely to be used)
+                # 2 = Vector4Half
+                return pos_count - 2
+            else:
+                # 3 = Vector3
+                # 4 = Vector4
+                return pos_count
+        if name == "weights":
+            return self.get_vector_type(7)
+        if name == "bones":
+            return 1
+        if name == "normal":
+            result = self.get_vector_type(0xB)
+            return result if result != 4 else 3
+        if name == "tangent":
+            result = self.get_vector_type(0xE)
+            return result if result != 4 else 3
+        if name == "unk":
+            result = self.get_vector_type(0x11)
+            return result if result != 4 else 3
+        if name == "diffuse":
+            return self.get_vector_type(0x16)
+        if name == "specular":
+            return self.get_vector_type(0x19)
+        if name == "uv":
+            if not vertex_packing & (1 << 0x1B):
+                # likely not to be UV
+                # treat as Vector2
+                return 1 << 16
+
+            # this assumes that uv_count can't be greater than 2
+            i = 0x1C
+            result = 0
+            uv_count = c_uint32(vertex_packing).value >> 0x1C
+            while uv_count > 0:
+                i += 4
+                shift = (vertex_packing >> i) & 0xF
+                if c_uint8(shift).value == 0xF:
+                    continue
+                uv_count -= 1
+                shift = (vertex_packing >> (i + 2)) & 3
+                if shift:
+                    if shift != 1:
+                        # add uv_count as index per type (highest comes first)
+                        # each 0x1 is a 4 * 8bit fixed point
+                        result += 1 + (uv_count << 4)
+                        continue
+                    # each 0x100 is a Vector2Half
+                    result += (1 << 8) + (uv_count << 12)
+                else:
+                    # each 0x10000 is a Vector2
+                    result += (1 << 16) + (uv_count << 20)
+            return result
 
     def get_vertex_layout(self) -> GMDVertexBufferLayout:
-        vertex_layout_bits = [
-            ("pos", 0x03),
-            ("weights", 0x3c0),
-            ("normal", 0x1c00),
-            ("tangent", 0xe000),
-
-            ("col0", 0x00e0_0000),
-            ("col1", 0x0700_0000),
-            ("uv",   0x1800_0000),
-        ]
-        overall_bitmask = 0
         vertex_elems = {}
-        for (name, bitmask) in vertex_layout_bits:
-            overall_bitmask = overall_bitmask | bitmask
-            vertex_elems[name] = self.all_bits(int(self.vertex_packing), bitmask)
-        # overall_bitmask should equal 0x1FE0_FFC3
-        # there should also be no bits set outside of the bitmask
+        vertex_layout_bits = [
+            ("pos", 0x07),
+            ("weights", 0x70),
+            ("bones", 0x200),
+            ("normal", 0x400),
+            ("tangent", 0x2000),
+            ("unk", 0x10000),
+            ("diffuse", 0x0020_0000),
+            ("specular", 0x0100_0000),
+            ("uv",   0xf000_0000),
+        ]
 
-        uv_bytes = 0
-        if self.uv_flags == 0x4:
-            uv_bytes = 2
-        elif self.uv_flags == 0x44:
-            uv_bytes = 4
+        for (name, bitmask) in vertex_layout_bits:
+            vertex_elems[name] = 0 if int(self.vertex_packing) & bitmask else -1
+        for (name) in vertex_elems:
+            if vertex_elems[name] == -1:
+                vertex_elems[name] = 0
+                continue
+            vertex_elems[name] = self.get_property_type(name)
 
         print(vertex_elems)
 
         return GMDVertexBufferLayout(
-            pos_en=vertex_elems["pos"],
-            weights_en=vertex_elems["weights"],
-            normal_en=vertex_elems["normal"],
-            tangent_en=vertex_elems["tangent"],
-            col0_en=vertex_elems["col0"],
-            col1_en=vertex_elems["col1"],
-            uv_en=vertex_elems["uv"],
-            uv_elem_bytes=uv_bytes
+            pos_type=vertex_elems["pos"],
+            weights_type=vertex_elems["weights"],
+            bones_en=vertex_elems["bones"],
+            normal_type=vertex_elems["normal"],
+            tangent_type=vertex_elems["tangent"],
+            unk_type=vertex_elems["unk"],
+            diffuse_type=vertex_elems["diffuse"],
+            specular_type=vertex_elems["specular"],
+            uv_type=vertex_elems["uv"],
         )
