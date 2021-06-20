0f5e4a24d89b2da7ee68e736d0ce610973506397
Samuel Stark <samuel@winslowstark.org>
2021-06-20 17:54:37 +0100

    Added comments to Mesh struct fields

--- yk_gmd_blender/yk_gmd/v2/structure/common/mesh.py
+++ yk_gmd_blender/yk_gmd/v2/structure/common/mesh.py
@@ -24,15 +24,23 @@ IndicesStruct_Unpack = StructureUnpacker(
 
 @dataclass(frozen=True)
 class MeshStruct:
+    # Index of the Mesh structure in the array
     index: int
+    # Mapped attribute set index
     attribute_index: int
+    # Vertex buffer index
     vertex_buffer_index: int
+    # Associated object index
     object_index: int
+    # object's associated node index
     node_index: int
 
+    # Minimum value of the associated indices in the index buffer
     min_index: int
+    # Number of vertices referenced
     vertex_count: int
-
+    # Index offset - if the index buffer references vertex X, we should remap it to reference vertex X + offset_from_index.
+    # Used to allow 16bit index buffers to reference all vertices in a vertex buffer with >65535 vertices
     vertex_offset_from_index: int
 
     # Each vertex in the mesh can be linked to up to 4 matrices.
