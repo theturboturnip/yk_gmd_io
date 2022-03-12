3fc0c13e4d72ca8dde178dcec5d1368ca49de7e9
Samuel Stark <samuel.winslow.stark@gmail.com>
2022-03-12 22:32:48 +0000

    Version bump

--- yk_gmd_blender/__init__.py
+++ yk_gmd_blender/__init__.py
@@ -2,7 +2,7 @@
 bl_info = {
     "name": "Yakuza GMD File Import/Export",
     "author": "Samuel Stark (TheTurboTurnip)",
-    "version": (0, 3, 2, 1),
+    "version": (0, 4, 0),
     "blender": (2, 92, 0),
     "location": "File > Import-Export",
     "description": "Import-Export Yakuza GMD Files",
