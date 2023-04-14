df550f353ff96eb809a95a5afedcc02935aa6473
Samuel Stark <samuel.winslow.stark@gmail.com>
2023-04-14 17:33:47 +0100

    Move more code out into index juggling

--- yk_gmd_blender/blender/export/mesh_exporter/index_juggling.py
+++ yk_gmd_blender/blender/export/mesh_exporter/index_juggling.py
@@ -1,7 +1,18 @@
-from typing import NewType, Tuple, List, Dict
+import array
+from dataclasses import dataclass
+from typing import List, Dict, Tuple, TypeVar, Callable, NewType
+
+from yk_gmd_blender.yk_gmd.v2.abstract.gmd_attributes import GMDAttributeSet
+from yk_gmd_blender.yk_gmd.v2.abstract.gmd_mesh import GMDSkinnedMesh, GMDMesh
+from yk_gmd_blender.yk_gmd.v2.abstract.gmd_shader import GMDVertexBuffer, GMDSkinnedVertexBuffer
+from yk_gmd_blender.yk_gmd.v2.abstract.nodes.gmd_bone import GMDBone
 
 # Int alias representing indices into bpy.types.Mesh().loops
 MeshLoopIdx = NewType("MeshLoopIdx", int)
+
+# Alias representing a triangle indexed into bpy.types.Mesh().loops.
+# Equivalent to (bpy.types.MeshLoopTriangle.loops) but doesn't need a blender import.
+MeshLoopTri = NewType("MeshLoopTri", Tuple[MeshLoopIdx, MeshLoopIdx, MeshLoopIdx])
 
 # Int alias representing offsets into material_vertices.
 # When creating meshes, we first split them by material and generate a vertex buffer `material_vertices`
@@ -59,3 +70,225 @@ def dedupe_loops(loops_with_dupes: List[MeshLoopIdx], vertex_bytes: List[bytes])
         loop_idx_to_deduped_verts_idx[loop_idx] = no_dupe_loop_idx
 
     return deduped_verts, loop_idx_to_deduped_verts_idx
+
+
+@dataclass(frozen=True)
+class Submesh:
+    """
+    A "submesh" - a minimally exportable chunk of a mesh.
+    Contains at most 65536 "vertices".
+    """
+
+    # The material for the submesh
+    attr_set: GMDAttributeSet
+    # The buffer of all vertices for that material
+    base_vertices: GMDVertexBuffer
+    # Indices of the data to turn into vertices.
+    verts: List[MaterialVertIdx]
+    # Triangle definitions, with indexes *that index into self.meshloop_idxs*
+    triangles: List[SubmeshTri]
+
+    def __post_init__(self):
+        if len(self.verts) > 65536:
+            raise RuntimeError("Created a Submesh with more than 65536 vertices.")
+
+    def build_unskinned(self) -> GMDMesh:
+        vertices = self.base_vertices.copy_scatter(self.verts)
+
+        triangle_list, triangle_strip_noreset, triangle_strip_reset = self._build_triangles()
+
+        return GMDMesh(
+            empty=False,
+            vertices_data=vertices,
+            triangle_indices=triangle_list,
+            triangle_strip_noreset_indices=triangle_strip_noreset,
+            triangle_strip_reset_indices=triangle_strip_reset,
+            attribute_set=self.attr_set
+        )
+
+    # Build the triangle strip from self.triangles
+    def _build_triangles(self) -> Tuple[array.ArrayType, array.ArrayType, array.ArrayType]:
+        triangle_list = array.array("H")
+        triangle_strip_noreset = array.array("H")
+        triangle_strip_reset = array.array("H")
+
+        for t0, t1, t2 in self.triangles:
+            triangle_list.append(t0)
+            triangle_list.append(t1)
+            triangle_list.append(t2)
+
+            # If we can continue the strip, do so
+            if not triangle_strip_noreset:
+                # Starting the strip
+                triangle_strip_noreset.append(t0)
+                triangle_strip_noreset.append(t1)
+                triangle_strip_noreset.append(t2)
+            elif (triangle_strip_noreset[-2] == t0 and
+                  triangle_strip_noreset[-1] == t1):
+                # Continue the strip
+                triangle_strip_noreset.append(t2)
+            else:
+                # End previous strip, start new strip
+                # Two extra verts to create a degenerate triangle, signalling the end of the strip
+                triangle_strip_noreset.append(triangle_strip_noreset[-1])
+                triangle_strip_noreset.append(t0)
+                # Add the triangle as normal
+                triangle_strip_noreset.append(t0)
+                triangle_strip_noreset.append(t1)
+                triangle_strip_noreset.append(t2)
+
+            # If we can continue the strip, do so
+            if not triangle_strip_reset:
+                # Starting the strip
+                triangle_strip_reset.append(t0)
+                triangle_strip_reset.append(t1)
+                triangle_strip_reset.append(t2)
+            elif (triangle_strip_reset[-2] == t0 and
+                  triangle_strip_reset[-1] == t1):
+                # Continue the strip
+                triangle_strip_reset.append(t2)
+            else:
+                # End previous strip, start new strip
+                # Reset index signalling the end of the strip
+                triangle_strip_reset.append(0xFFFF)
+                # Add the triangle as normal
+                triangle_strip_reset.append(t0)
+                triangle_strip_reset.append(t1)
+                triangle_strip_reset.append(t2)
+
+        return triangle_list, triangle_strip_noreset, triangle_strip_reset
+
+
+TSubmesh = TypeVar("TSubmesh", bound=Submesh)
+
+
+def convert_meshloop_tris_to_tsubmeshes(
+        deduped_verts: List[MaterialVertIdx],
+        loop_idx_to_deduped_verts_idx: Dict[MeshLoopIdx, DedupedVertIdx],
+        triangles: List[MeshLoopTri],
+        submesh_generator: Callable[
+            [
+                List[MaterialVertIdx],
+                List[SubmeshTri]
+            ],
+            TSubmesh
+        ]
+) -> List[TSubmesh]:
+    submeshes = []
+
+    deduped_verts_idx_to_pending_vert_idx: Dict[DedupedVertIdx, SubmeshVertIdx] = {}
+    pending_verts: List[MaterialVertIdx] = []
+    pending_tris: List[SubmeshTri] = []
+
+    def get_or_insert_pending_vert(deduped_verts_idx: DedupedVertIdx) -> SubmeshVertIdx:
+        nonlocal pending_verts, pending_tris, deduped_verts_idx_to_pending_vert_idx
+        # See if this deduped vertex has already appeared in the pending submesh
+        pending_idx = deduped_verts_idx_to_pending_vert_idx.get(deduped_verts_idx)
+        if pending_idx is None:
+            # if it hasn't, push it to pending_verts...
+            pending_idx = SubmeshVertIdx(len(pending_verts))
+            pending_verts.append(deduped_verts[deduped_verts_idx])
+            # ... and register its index
+            deduped_verts_idx_to_pending_vert_idx[deduped_verts_idx] = pending_idx
+        # Either way, the deduped vert is definitely in the pending submesh now.
+        return pending_idx
+
+    def push_submesh_and_reset_pending():
+        nonlocal pending_verts, pending_tris, deduped_verts_idx_to_pending_vert_idx
+        submeshes.append(submesh_generator(pending_verts, pending_tris))
+        pending_verts = []
+        pending_tris = []
+        deduped_verts_idx_to_pending_vert_idx = {}
+
+    for t in triangles:
+        t_no_dupes = (
+            loop_idx_to_deduped_verts_idx[t[0]],
+            loop_idx_to_deduped_verts_idx[t[1]],
+            loop_idx_to_deduped_verts_idx[t[2]],
+        )
+
+        # We have a maximum of 65536 vertices.
+        # At most, adding a new triangle can only add 3 loops to the "pending" buffer.
+        # also, adding a triangle may add 0 loops - if they're all used already.
+        # So if we have 65533 loops, check to see how many we would add.
+        if len(pending_verts) >= (65536 - 3):
+            # We have to be careful, we might grow beyond the buffer
+            num_to_add = 0
+            if deduped_verts[t_no_dupes[0]] not in deduped_verts_idx_to_pending_vert_idx:
+                num_to_add += 1
+            if deduped_verts[t_no_dupes[1]] not in deduped_verts_idx_to_pending_vert_idx:
+                num_to_add += 1
+            if deduped_verts[t_no_dupes[2]] not in deduped_verts_idx_to_pending_vert_idx:
+                num_to_add += 1
+            if len(pending_verts) + num_to_add > 65536:
+                # Push the current loops into a Submesh struct and reset the pending
+                push_submesh_and_reset_pending()
+        # We can add any triangle to the buffer, it's guaranteed to result in a buffer with <65536 loops
+        pending_tris.append(SubmeshTri((
+            get_or_insert_pending_vert(t_no_dupes[0]),
+            get_or_insert_pending_vert(t_no_dupes[1]),
+            get_or_insert_pending_vert(t_no_dupes[2]),
+        )))
+
+    if pending_verts or pending_tris:
+        push_submesh_and_reset_pending()
+
+    if len(submeshes) > 2:
+        print(f"WOOOOOO MANY ({len(submeshes)}) SUBMESHES")
+
+    return submeshes
+
+
+def convert_meshloop_tris_to_unskinned_submeshes(
+        attr_set: GMDAttributeSet,
+        base_vertices: GMDVertexBuffer,
+        deduped_verts: List[MaterialVertIdx],
+        loop_idx_to_deduped_verts_idx: Dict[MeshLoopIdx, DedupedVertIdx],
+        triangles: List[MeshLoopTri],
+) -> List[Submesh]:
+    return convert_meshloop_tris_to_tsubmeshes(
+        deduped_verts,
+        loop_idx_to_deduped_verts_idx,
+        triangles,
+        lambda loops, triangles: Submesh(attr_set, base_vertices, loops, triangles)
+    )
+
+
+@dataclass(frozen=True)
+class SkinnedSubmesh(Submesh):
+    """
+    A submesh with extra data for skinning e.g. vertex groups.
+    """
+    # The base vertex buffer must be skinned
+    base_vertices: GMDSkinnedVertexBuffer
+    # A mapping of (blender vertex group) -> (local bone index).
+    relevant_vertex_groups: Dict[int, int]
+    # A mapping of (local bone index) -> (relevant GMD bone)
+    relevant_bones: List[GMDBone]
+
+    def build_skinned(self) -> GMDSkinnedMesh:
+        # Copy vertices out of vertex buffer
+        vertices = self.base_vertices.copy_scatter(self.verts)
+        # Where vertices.weight_data > 0, remap vertices.bone_data with self.relevant_vertex_groups
+        for i in range(len(vertices)):
+            for j in range(4):
+                if vertices.weight_data[i, j] > 0:
+                    if vertices.bone_data[i, j] not in self.relevant_vertex_groups:
+                        print(i, j,
+                              self.verts[i], vertices.bone_data[i], vertices.weight_data[i],
+                              self.base_vertices.bone_data[self.verts[i]],
+                              self.base_vertices.weight_data[self.verts[i]],
+                              list(self.relevant_vertex_groups.keys()))
+                    vertices.bone_data[i, j] = self.relevant_vertex_groups[vertices.bone_data[i, j]]
+
+        triangle_list, triangle_strip_noreset, triangle_strip_reset = self._build_triangles()
+
+        return GMDSkinnedMesh(
+            empty=False,
+            vertices_data=vertices,
+            triangle_indices=triangle_list,
+            triangle_strip_noreset_indices=triangle_strip_noreset,
+            triangle_strip_reset_indices=triangle_strip_reset,
+            attribute_set=self.attr_set,
+            relevant_bones=self.relevant_bones
+        )
--- yk_gmd_blender/blender/export/mesh_exporter/v2.py
+++ yk_gmd_blender/blender/export/mesh_exporter/v2.py
@@ -1,6 +1,4 @@
-import array
-from dataclasses import dataclass
-from typing import List, Dict, Tuple, Set, TypeVar, Callable
+from typing import List, Dict, Set, Tuple
 from typing import cast
 
 import numpy as np
@@ -9,11 +7,12 @@ import bpy
 from yk_gmd_blender.blender.export.mesh_exporter.extractor import compute_vertex_4weights, loop_indices_for_material, \
     extract_vertices_for_skinned_material, generate_vertex_byteslices, \
     extract_vertices_for_unskinned_material
-from yk_gmd_blender.blender.export.mesh_exporter.index_juggling import dedupe_loops, MaterialVertIdx, SubmeshTri, \
-    DedupedVertIdx, SubmeshVertIdx, MeshLoopIdx
+from yk_gmd_blender.blender.export.mesh_exporter.index_juggling import dedupe_loops, SkinnedSubmesh, \
+    convert_meshloop_tris_to_unskinned_submeshes, Submesh, convert_meshloop_tris_to_tsubmeshes, MeshLoopTri, \
+    MeshLoopIdx, DedupedVertIdx, MaterialVertIdx
 from yk_gmd_blender.yk_gmd.v2.abstract.gmd_attributes import GMDAttributeSet
 from yk_gmd_blender.yk_gmd.v2.abstract.gmd_mesh import GMDSkinnedMesh, GMDMesh
-from yk_gmd_blender.yk_gmd.v2.abstract.gmd_shader import GMDVertexBuffer, GMDSkinnedVertexBuffer
+from yk_gmd_blender.yk_gmd.v2.abstract.gmd_shader import GMDSkinnedVertexBuffer
 from yk_gmd_blender.yk_gmd.v2.abstract.nodes.gmd_bone import GMDBone
 from yk_gmd_blender.yk_gmd.v2.errors.error_reporter import ErrorReporter
 
@@ -56,14 +55,14 @@ def split_skinned_blender_mesh_object(context: bpy.types.Context, object: bpy.ty
             deduped_verts,
             loop_idx_to_deduped_verts_idx,
             # Relevant triangles
-            [t for t in mesh.loop_triangles if t.material_index == attr_set_idx],
+            [MeshLoopTri(t.loops) for t in mesh.loop_triangles if t.material_index == attr_set_idx],
             # Per-mesh bone info TODO should use this instead of mesh
             bone_info,
             vertex_group_mapping,
             # Error reporting
             error,
             # Config
-            max_bones_per_submesh=255  # bone_limit
+            max_bones_per_submesh=bone_limit
         )
 
     return [s.build_skinned() for s in skinned_submeshes]
@@ -89,7 +88,7 @@ def split_unskinned_blender_mesh_object(context: bpy.types.Context, object: bpy.
             base_vertices,
             deduped_verts,
             loop_idx_to_deduped_verts_idx,
-            [t for t in mesh.loop_triangles if t.material_index == attr_set_idx],
+            [MeshLoopTri(t.loops) for t in mesh.loop_triangles if t.material_index == attr_set_idx],
         )
 
     return [s.build_unskinned() for s in submeshes]
@@ -126,232 +125,13 @@ def check_needs_tangent(materials: List[GMDAttributeSet]) -> bool:
     return any(m.shader.vertex_buffer_layout.tangent_storage for m in materials)
 
 
-@dataclass(frozen=True)
-class Submesh:
-    """
-    A "submesh" - a minimally exportable chunk of a mesh.
-    Contains at most 65536 "vertices".
-    """
-
-    # The material for the submesh
-    attr_set: GMDAttributeSet
-    # The buffer of all vertices for that material
-    base_vertices: GMDVertexBuffer
-    # Indices of the data to turn into vertices.
-    verts: List[MaterialVertIdx]
-    # Triangle definitions, with indexes *that index into self.meshloop_idxs*
-    triangles: List[SubmeshTri]
-
-    def __post_init__(self):
-        if len(self.verts) > 65536:
-            raise RuntimeError("Created a Submesh with more than 65536 vertices.")
-
-    def build_unskinned(self) -> GMDMesh:
-        vertices = self.base_vertices.copy_scatter(self.verts)
-
-        triangle_list, triangle_strip_noreset, triangle_strip_reset = self._build_triangles()
-
-        return GMDMesh(
-            empty=False,
-            vertices_data=vertices,
-            triangle_indices=triangle_list,
-            triangle_strip_noreset_indices=triangle_strip_noreset,
-            triangle_strip_reset_indices=triangle_strip_reset,
-            attribute_set=self.attr_set
-        )
-
-    # Build the triangle strip from self.triangles
-    def _build_triangles(self) -> Tuple[array.ArrayType, array.ArrayType, array.ArrayType]:
-        triangle_list = array.array("H")
-        triangle_strip_noreset = array.array("H")
-        triangle_strip_reset = array.array("H")
-
-        for t0, t1, t2 in self.triangles:
-            triangle_list.append(t0)
-            triangle_list.append(t1)
-            triangle_list.append(t2)
-
-            # If we can continue the strip, do so
-            if not triangle_strip_noreset:
-                # Starting the strip
-                triangle_strip_noreset.append(t0)
-                triangle_strip_noreset.append(t1)
-                triangle_strip_noreset.append(t2)
-            elif (triangle_strip_noreset[-2] == t0 and
-                  triangle_strip_noreset[-1] == t1):
-                # Continue the strip
-                triangle_strip_noreset.append(t2)
-            else:
-                # End previous strip, start new strip
-                # Two extra verts to create a degenerate triangle, signalling the end of the strip
-                triangle_strip_noreset.append(triangle_strip_noreset[-1])
-                triangle_strip_noreset.append(t0)
-                # Add the triangle as normal
-                triangle_strip_noreset.append(t0)
-                triangle_strip_noreset.append(t1)
-                triangle_strip_noreset.append(t2)
-
-            # If we can continue the strip, do so
-            if not triangle_strip_reset:
-                # Starting the strip
-                triangle_strip_reset.append(t0)
-                triangle_strip_reset.append(t1)
-                triangle_strip_reset.append(t2)
-            elif (triangle_strip_reset[-2] == t0 and
-                  triangle_strip_reset[-1] == t1):
-                # Continue the strip
-                triangle_strip_reset.append(t2)
-            else:
-                # End previous strip, start new strip
-                # Reset index signalling the end of the strip
-                triangle_strip_reset.append(0xFFFF)
-                # Add the triangle as normal
-                triangle_strip_reset.append(t0)
-                triangle_strip_reset.append(t1)
-                triangle_strip_reset.append(t2)
-
-        return triangle_list, triangle_strip_noreset, triangle_strip_reset
-
-
-TSubmesh = TypeVar("TSubmesh", bound=Submesh)
-
-
-def convert_meshloop_tris_to_tsubmeshes(
-        deduped_verts: List[MaterialVertIdx],
-        loop_idx_to_deduped_verts_idx: Dict[MeshLoopIdx, DedupedVertIdx],
-        triangles: List[bpy.types.MeshLoopTriangle],
-        submesh_generator: Callable[
-            [
-                List[MaterialVertIdx],
-                List[SubmeshTri]
-            ],
-            TSubmesh
-        ]
-) -> List[TSubmesh]:
-    submeshes = []
-
-    deduped_verts_idx_to_pending_vert_idx: Dict[DedupedVertIdx, SubmeshVertIdx] = {}
-    pending_verts: List[MaterialVertIdx] = []
-    pending_tris: List[SubmeshTri] = []
-
-    def get_or_insert_pending_vert(deduped_verts_idx: DedupedVertIdx) -> SubmeshVertIdx:
-        nonlocal pending_verts, pending_tris, deduped_verts_idx_to_pending_vert_idx
-        # See if this deduped vertex has already appeared in the pending submesh
-        pending_idx = deduped_verts_idx_to_pending_vert_idx.get(deduped_verts_idx)
-        if pending_idx is None:
-            # if it hasn't, push it to pending_verts...
-            pending_idx = SubmeshVertIdx(len(pending_verts))
-            pending_verts.append(deduped_verts[deduped_verts_idx])
-            # ... and register its index
-            deduped_verts_idx_to_pending_vert_idx[deduped_verts_idx] = pending_idx
-        # Either way, the deduped vert is definitely in the pending submesh now.
-        return pending_idx
-
-    def push_submesh_and_reset_pending():
-        nonlocal pending_verts, pending_tris, deduped_verts_idx_to_pending_vert_idx
-        submeshes.append(submesh_generator(pending_verts, pending_tris))
-        pending_verts = []
-        pending_tris = []
-        deduped_verts_idx_to_pending_vert_idx = {}
-
-    for t in triangles:
-        t_no_dupes = (
-            loop_idx_to_deduped_verts_idx[MeshLoopIdx(t.loops[0])],
-            loop_idx_to_deduped_verts_idx[MeshLoopIdx(t.loops[1])],
-            loop_idx_to_deduped_verts_idx[MeshLoopIdx(t.loops[2])],
-        )
-
-        # We have a maximum of 65536 vertices.
-        # At most, adding a new triangle can only add 3 loops to the "pending" buffer.
-        # also, adding a triangle may add 0 loops - if they're all used already.
-        # So if we have 65533 loops, check to see how many we would add.
-        if len(pending_verts) >= (65536 - 3):
-            # We have to be careful, we might grow beyond the buffer
-            num_to_add = 0
-            if deduped_verts[t_no_dupes[0]] not in deduped_verts_idx_to_pending_vert_idx:
-                num_to_add += 1
-            if deduped_verts[t_no_dupes[1]] not in deduped_verts_idx_to_pending_vert_idx:
-                num_to_add += 1
-            if deduped_verts[t_no_dupes[2]] not in deduped_verts_idx_to_pending_vert_idx:
-                num_to_add += 1
-            if len(pending_verts) + num_to_add > 65536:
-                # Push the current loops into a Submesh struct and reset the pending
-                push_submesh_and_reset_pending()
-        # We can add any triangle to the buffer, it's guaranteed to result in a buffer with <65536 loops
-        pending_tris.append(SubmeshTri((
-            get_or_insert_pending_vert(t_no_dupes[0]),
-            get_or_insert_pending_vert(t_no_dupes[1]),
-            get_or_insert_pending_vert(t_no_dupes[2]),
-        )))
-
-    if pending_verts or pending_tris:
-        push_submesh_and_reset_pending()
-
-    return submeshes
-
-
-def convert_meshloop_tris_to_unskinned_submeshes(
-        attr_set: GMDAttributeSet,
-        base_vertices: GMDVertexBuffer,
-        deduped_verts: List[MaterialVertIdx],
-        loop_idx_to_deduped_verts_idx: Dict[MeshLoopIdx, DedupedVertIdx],
-        triangles: List[bpy.types.MeshLoopTriangle],
-) -> List[Submesh]:
-    return convert_meshloop_tris_to_tsubmeshes(
-        deduped_verts,
-        loop_idx_to_deduped_verts_idx,
-        triangles,
-        lambda loops, triangles: Submesh(attr_set, base_vertices, loops, triangles)
-    )
-
-
-@dataclass(frozen=True)
-class SkinnedSubmesh(Submesh):
-    """
-    A submesh with extra data for skinning e.g. vertex groups.
-    """
-    # The base vertex buffer must be skinned
-    base_vertices: GMDSkinnedVertexBuffer
-    # A mapping of (blender vertex group) -> (local bone index).
-    relevant_vertex_groups: Dict[int, int]
-    # A mapping of (local bone index) -> (relevant GMD bone)
-    relevant_bones: List[GMDBone]
-
-    def build_skinned(self) -> GMDSkinnedMesh:
-        # Copy vertices out of vertex buffer
-        vertices = self.base_vertices.copy_scatter(self.verts)
-        # Where vertices.weight_data > 0, remap vertices.bone_data with self.relevant_vertex_groups
-        for i in range(len(vertices)):
-            for j in range(4):
-                if vertices.weight_data[i, j] > 0:
-                    if vertices.bone_data[i, j] not in self.relevant_vertex_groups:
-                        print(i, j,
-                              self.verts[i], vertices.bone_data[i], vertices.weight_data[i],
-                              self.base_vertices.bone_data[self.verts[i]],
-                              self.base_vertices.weight_data[self.verts[i]],
-                              list(self.relevant_vertex_groups.keys()))
-                    vertices.bone_data[i, j] = self.relevant_vertex_groups[vertices.bone_data[i, j]]
-
-        triangle_list, triangle_strip_noreset, triangle_strip_reset = self._build_triangles()
-
-        return GMDSkinnedMesh(
-            empty=False,
-            vertices_data=vertices,
-            triangle_indices=triangle_list,
-            triangle_strip_noreset_indices=triangle_strip_noreset,
-            triangle_strip_reset_indices=triangle_strip_reset,
-            attribute_set=self.attr_set,
-            relevant_bones=self.relevant_bones
-        )
-
-
 def convert_meshloop_tris_to_skinned_submeshes(
         attr_set: GMDAttributeSet,
         base_vertices: GMDSkinnedVertexBuffer,
         mesh: bpy.types.Mesh,
         deduped_verts: List[MaterialVertIdx],
         loop_idx_to_deduped_verts_idx: Dict[MeshLoopIdx, DedupedVertIdx],
-        triangles: List[bpy.types.MeshLoopTriangle],
+        triangles: List[MeshLoopTri],
         bone_info: Tuple[np.ndarray, np.ndarray, np.ndarray],
         vertex_group_mapping: Dict[int, GMDBone],
         error: ErrorReporter,
@@ -363,10 +143,10 @@ def convert_meshloop_tris_to_skinned_submeshes(
 
     vert_groups, weights, n_weights = bone_info
 
-    def find_triangle_vertex_groups(t: bpy.types.MeshLoopTriangle) -> Set[int]:
-        v0 = mesh.loops[t.loops[0]].vertex_index
-        v1 = mesh.loops[t.loops[1]].vertex_index
-        v2 = mesh.loops[t.loops[2]].vertex_index
+    def find_triangle_vertex_groups(t: MeshLoopTri) -> Set[int]:
+        v0 = mesh.loops[t[0]].vertex_index
+        v1 = mesh.loops[t[1]].vertex_index
+        v2 = mesh.loops[t[2]].vertex_index
 
         vgs = set(vert_groups[v0, 0:n_weights[v0]])
         vgs.update(vert_groups[v1, 0:n_weights[v1]])
@@ -376,10 +156,10 @@ def convert_meshloop_tris_to_skinned_submeshes(
     # First, partition the triangles into groups that use at most max_bones_per_submesh bones.
     # Don't remap the triangle indices yet
     triangle_partitions: List[Tuple[
-        List[bpy.types.MeshLoopTriangle],  # A group of raw Blender triangles
+        List[MeshLoopTri],  # A group of raw Blender triangles
         Set[int]  # the vertex groups they reference
     ]] = []
-    pending_tris: List[bpy.types.MeshLoopTriangle] = []
+    pending_tris: List[MeshLoopTri] = []
     pending_vgs: Set[int] = set()
     for t in triangles:
         triangle_vgs = find_triangle_vertex_groups(t)
