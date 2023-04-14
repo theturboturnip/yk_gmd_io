a9b9e0d89afb99d2c385e0ad63aea86fccba86b0
Samuel Stark <samuel.winslow.stark@gmail.com>
2023-04-14 17:33:22 +0100

    Pull non-blender-dependent code into separate file and test

--- test/test_mesh_exporter.py
+++ test/test_mesh_exporter.py
@@ -61,3 +61,33 @@ def test_dedupe_with_no_dupes_with_material_offsets():
         104: 4,
         105: 5,
     }
+
+
+@pytest.mark.order(3)
+def test_dedupe_with_dupes_with_material_offsets():
+    loops_with_dupes = [
+        MeshLoopIdx(100),
+        MeshLoopIdx(101),
+        MeshLoopIdx(102),
+        MeshLoopIdx(103),
+        MeshLoopIdx(104),
+        MeshLoopIdx(105),
+    ]
+    vertices = [
+        bytes([0]),
+        bytes([0]),
+        bytes([0]),
+        bytes([1]),
+        bytes([1]),
+        bytes([1]),
+    ]
+    deduped_verts, deduped_dict = dedupe_loops(loops_with_dupes, vertices)
+    assert deduped_verts == [0, 3]
+    assert deduped_dict == {
+        100: 0,
+        101: 0,
+        102: 0,
+        103: 1,
+        104: 1,
+        105: 1,
+    }
