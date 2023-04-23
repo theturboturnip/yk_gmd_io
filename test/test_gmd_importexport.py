3971e62515a9e0f912b6065ec1c5f9f263c48975
Samuel Stark <samuel.winslow.stark@gmail.com>
2023-04-23 19:34:36 +0100

    Add testing for animation import
    This cannot test the imported contents, but it makes sure the anim import doesn't crash.

--- test/test_gmd_importexport.py
+++ test/test_gmd_importexport.py
@@ -37,7 +37,43 @@ COMPARE_FILTER = {
 
 
 @pytest.mark.order(10)
-def test_gmd_importexport_comparelenient(gmdtest: GMDTest, blender: Path, isolate_blender: bool):
+def test_gmd(gmdtest: GMDTest, blender: Path, isolate_blender: bool):
+    if gmdtest.animation:
+        gmd_importanim(gmdtest, blender, isolate_blender)
+    else:
+        gmd_importexport_comparelenient(gmdtest, blender, isolate_blender)
+
+
+def gmd_importanim(gmdtest: GMDTest, blender: Path, isolate_blender: bool):
+    # Set the src/dst for the import/export
+    env = os.environ.copy()
+    env.update({
+        "YKGMDIO_TEST_SRC": str(gmdtest.src),
+        "YKGMDIO_SKINNED": str(gmdtest.skinned_method),
+        "YKGMDIO_LOGGING": gmdtest.logging,
+    })
+
+    SCRIPTLOC = os.path.dirname(__file__)
+
+    # Run blender and import/export the file
+    if isolate_blender:
+        subprocess.run([
+            str(blender / "blender"),
+            "--factory-startup",
+            "-b",
+            "--python-exit-code", "1",
+            "-P", f"{SCRIPTLOC}/blender_do_importanim.py"
+        ], check=True, env=env)
+    else:
+        subprocess.run([
+            str(blender / "blender"),
+            "-b",
+            "--python-exit-code", "1",
+            "-P", f"{SCRIPTLOC}/blender_do_importanim.py"
+        ], check=True, env=env)
+
+
+def gmd_importexport_comparelenient(gmdtest: GMDTest, blender: Path, isolate_blender: bool):
     # Create subfolder in output for directory
     gmdtest.dst.parent.mkdir(parents=True, exist_ok=True)
     # Copy the file into the output - this should be overwritten by blender
