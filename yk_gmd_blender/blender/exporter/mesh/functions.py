9456e015b97925f2ac7bf310e16fd3f6cb0e8eeb
Samuel Stark <samuel.winslow.stark@gmail.com>
2023-12-16 22:43:50 +0000

    Improve error message for triangulation fail
    Tangent calculation only seems to fail on meshes with N-gons, so catch that error and rethrow with
    a) context of what object it is
    b) how to fix (add the triangulate modifier)

--- yk_gmd_blender/blender/exporter/mesh/functions.py
+++ yk_gmd_blender/blender/exporter/mesh/functions.py
@@ -12,6 +12,7 @@ from yk_gmd_blender.gmdlib.abstract.gmd_attributes import GMDAttributeSet
 from yk_gmd_blender.gmdlib.abstract.gmd_mesh import GMDSkinnedMesh, GMDMesh, GMDMeshIndices
 from yk_gmd_blender.gmdlib.abstract.gmd_shader import GMDSkinnedVertexBuffer
 from yk_gmd_blender.gmdlib.abstract.nodes.gmd_bone import GMDBone
+from yk_gmd_blender.gmdlib.errors.error_classes import GMDImportExportError
 from yk_gmd_blender.gmdlib.errors.error_reporter import ErrorReporter
 from yk_gmd_blender.meshlib.export_submeshing import dedupe_loops, \
     convert_meshloop_tris_to_tsubmeshes, MeshLoopTri, \
@@ -114,7 +115,11 @@ def prepare_mesh(context: bpy.types.Context, object: bpy.types.Object, needs_tan
     # Now it's triangulated we can calculate tangents (TODO calc_normals_split may not be necessary anymore)
     bpy_mesh.calc_normals_split()
     if needs_tangent:
-        bpy_mesh.calc_tangents()
+        try:
+            bpy_mesh.calc_tangents()
+        except RuntimeError as err:
+            raise GMDImportExportError(
+                f"Failed to calculate tangents for object '{object.name_full}'. Try adding the triangulate modifier to this object to fix it.\n{err}")
 
     # TODO assuming this triangulates automatically
     bpy_mesh.calc_loop_triangles()
